9c301582001d04f98dcc4545561beaca47b30342
Santosh Kumar <santoshkumar15novmth@gmail.com>
2026-03-17 23:40:26 +0530

    Added bcrypt

--- services/user_service.go
+++ services/user_service.go
@@ -3,7 +3,9 @@ package services
 import (
 	db "GoTwitter/db/repositories"
 	"GoTwitter/models"
+	"GoTwitter/utils"
 	"context"
+	"errors"
 	"fmt"
 )
 
@@ -23,5 +25,18 @@ func NewUserService(_userRepository db.UserRepository) UserService {
 
 func (u *UserServiceImpl) CreateUser(ctx context.Context, user *models.User) (*models.User, error) {
 	fmt.Println("Creating user in userService")
+	if user == nil {
+		return nil, errors.New("user is nil")
+	}
+	if user.Password == "" {
+		return nil, errors.New("password is required")
+	}
+
+	hashed, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+	user.Password = string(hashed)
+
 	return u.userRepository.Create(ctx, user)
 }
