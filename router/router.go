e1ea2ee59759c29510f3b1517b2ec805f5b533df
Santosh Kumar <santoshkumar15novmth@gmail.com>
2026-03-22 23:26:47 +0530

    Added cors

--- router/router.go
+++ router/router.go
@@ -4,6 +4,8 @@ import (
 	"GoTwitter/controllers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type Router interface {
@@ -11,13 +13,24 @@ type Router interface {
 }
 
 func SetupRouter(UserRouter Router, TweetRouter Router, TagRouter Router) *chi.Mux {
-        chiRouter := chi.NewRouter()
+	chiRouter := chi.NewRouter()
 
-        chiRouter.Get("/ping", controllers.PingHandler)
+	chiRouter.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
-        UserRouter.Register(chiRouter)
-        TweetRouter.Register(chiRouter)
-        TagRouter.Register(chiRouter)
+	chiRouter.Use(middleware.Logger)
 
-        return chiRouter
+	chiRouter.Get("/ping", controllers.PingHandler)
+
+	UserRouter.Register(chiRouter)
+	TweetRouter.Register(chiRouter)
+	TagRouter.Register(chiRouter)
+
+	return chiRouter
 }
